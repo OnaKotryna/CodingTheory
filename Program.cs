db18c9fc2ce55c6c98abefdd9b01172d7e938986
Ona Kotryna <ok.kuncyte@gmail.com>
2021-11-30 11:49:35 +0200

    Get probability and vector functions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Coding_Theory
 {
@@ -6,7 +9,47 @@ namespace Coding_Theory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Užduotis A15");
+
+            double probabilityNumber = 0;
+            bool getProbability = true;
+
+            // Klaidos tikimybes ivedimas
+            while (getProbability)
+            {
+                Console.WriteLine("Nurodykite klaidos tikimybę:");
+                string givenProbability = Console.ReadLine();
+                givenProbability = givenProbability.Replace(',', '.');
+                if (Double.TryParse(givenProbability, out probabilityNumber) && probabilityNumber < 1)
+                {
+                    getProbability = false;
+                }
+                else
+                {
+                    Console.WriteLine("Klaida tikimybės nurodyme.", givenProbability);
+                }
+            }
+
+            // Scenarijaus pasirinkimas
+            /*Console.WriteLine("Pasirinkite scenarijų:\n1 - Vektoriaus siuntimas"); // \n2 - teksto siuntmas\n3 - paveiksliuko siuntimas
+            int selected = Int32.Parse(Console.ReadLine());
+            switch (selected)
+            {
+                case 1:
+                    SelectionHandler.HandleVector();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("pasirinkimas nerastas");
+                    break;
+            }*/
+
+            SelectionHandler.HandleVector();
+            
         }
+        
     }
 }
---
+++ SelectionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Coding_Theory
+{
+    static class SelectionHandler
+    {
+
+        // Ivedamas vartotojo vektorius
+        // ir siunciamas kodavimui
+        public static void HandleVector()
+        {
+            bool getVector = true;
+            int[] vector;
+            while (getVector)
+            {
+                Console.WriteLine("Iveskite dvejetaini vektoriu: \nPvz. 010010");
+                string givenVector = Console.ReadLine();
+                if (Regex.IsMatch(givenVector, "^[01]+$"))
+                {
+                    vector = new int[givenVector.Length];
+                    for (int i = 0; i < givenVector.Length; i++)
+                    {
+                        vector[i] = Convert.ToInt32(givenVector[i].ToString());
+                    }
+                    getVector = false;
+                }
+                else
+                {
+                    Console.WriteLine("\nVektorius nera dvejetainis\n");
+                }
+            }
+        }
+
+    }
+}
